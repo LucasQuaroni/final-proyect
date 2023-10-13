4c033c983d9eef5111d467b6683459e35683fb89
Lucas Quaroni <lucas.quaroni@gmail.com>
2023-10-13 13:01:37 -0300

    login completado

--- login/autenticarLogin.php
+++ login/autenticarLogin.php
@@ -1,22 +1,33 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "empresa");
+session_start();
+
+$conexion = mysqli_connect("localhost", "root", "", "fornaxpost");
 $usuario = mysqli_real_escape_string($conexion, $_POST['user']);
 $contra = mysqli_real_escape_string($conexion, $_POST['pass']);
 
 if ($usuario != "" && $contra != "") {
-    $consulta = "SELECT * FROM usuario WHERE usuario='$usuario' AND contraseña='$contra'";
+    $consulta = "SELECT * FROM usuarios WHERE usuario='$usuario' AND contra='$contra'";
     $resultado = mysqli_query($conexion, $consulta);
     $filas = mysqli_num_rows($resultado);
 
     if ($filas > 0) {
-        $consulta_perfil = "SELECT perfil FROM usuario WHERE usuario='$usuario' AND contraseña='$contra'";
-        $resultado_perfil = mysqli_query($conexion, $consulta_perfil);
-        $perfil = mysqli_fetch_assoc($resultado_perfil)['perfil'];
+        $consulta_rol = "SELECT rol FROM usuarios WHERE usuario='$usuario' AND contra='$contra'";
+        $resultado_rol = mysqli_query($conexion, $consulta_rol);
+        $rol = mysqli_fetch_assoc($resultado_rol)['rol'];
 
-        if ($perfil == 'A') {
-            header("location:../admin/admin.html");
-        } elseif ($perfil == 'C') {
-            header("location:../chofer/chofer.html");
+        // Inicia la sesión y establece la variable de sesión
+        $_SESSION['usuario'] = $usuario;
+        $_SESSION['rol'] = $rol;
+
+        if ($rol == 'A') {
+            header("Location: ../admin/admin.php");
+            exit;
+        } elseif ($rol == 'C') {
+            header("Location: ../chofer/chofer.php");
+            exit;
+        } elseif ($rol == 'T') {
+            header("Location: ../tecnico/tecnico.php");
+            exit;
         } else {
             echo "Error en la autenticación";
         }
