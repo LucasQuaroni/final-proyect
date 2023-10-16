f24e139fcb183e120922500d10e9945bec527b3a
Lucas Quaroni <lucas.quaroni@enerminds.com>
2023-10-16 16:55:45 -0300

    bd actualizada, con varios inputs. limpiar

--- resources/fornaxpost.sql
+++ resources/fornaxpost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2023 a las 17:57:17
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 16-10-2023 a las 21:54:47
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,7 @@ CREATE TABLE `artefactos` (
   `serial` varchar(15) NOT NULL,
   `modelo` varchar(30) DEFAULT NULL,
   `garantia` enum('S','N') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `artefactos`
@@ -40,7 +40,13 @@ CREATE TABLE `artefactos` (
 INSERT INTO `artefactos` (`serial`, `modelo`, `garantia`) VALUES
 ('12321', NULL, NULL),
 ('12989898', 'FORNAX FIT 50', 'S'),
-('23132132131', 'FORNAX FIT 50', 'S');
+('23132132131', 'FORNAX FIT 50', 'S'),
+('2313213999', NULL, NULL),
+('434', 'modelo_artefacto', ''),
+('4344', 'asd prueba 1', 'S'),
+('44442', NULL, NULL),
+('444422131', NULL, NULL),
+('444422131323', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,14 +64,21 @@ CREATE TABLE `clientes` (
   `localidad` varchar(30) NOT NULL,
   `codpostal` varchar(15) NOT NULL,
   `obs` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
 INSERT INTO `clientes` (`dni`, `nombreYapellido`, `domicilio`, `telefono`, `email`, `provincia`, `localidad`, `codpostal`, `obs`) VALUES
-('44765283', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Nada que observar');
+('1', 'yo', 'asd', 'asd', 'asda@gmail.com', 'sda', 'asd', 'ads', 'asdada'),
+('22223', 'marvo', 'asd', 'asd', 'asd@gmail.com', 'asd', '|asd', 'ads', 'asdsa'),
+('34243', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Cliente nuevo'),
+('44232238', 'Marcelo Benitez', 'Balbo 4156', '3415690470', 'marcebenitez0607@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Marcelo el nene'),
+('44523085', 'Emma Procaccini', 'Avenida del huerto 1223', '3415690470', 'emmaproca@icloud.com', 'Santa Fe', 'Rosario', '2000', 'Emma nueva cliente top'),
+('44765222', 'asd', 'asd', 'asd', 'asda@gmail.com', 'asntasda', 'dsa', 'dsa', 'dsasda'),
+('44765283', 'Lucas Quaroni', 'Alvear 12', '3416956364', 'lucas.quaroni@gmail.com', 'Santa Fe', 'Rosario', '2000', 'Nada que observar'),
+('888', 'Maria', 'adad', '232213', 'dsadsa@gmail.com', 'dsadsa', 'dadqs', 'dadwa', 'dsadwa');
 
 -- --------------------------------------------------------
 
@@ -77,7 +90,7 @@ CREATE TABLE `estados` (
   `idestado` varchar(3) NOT NULL,
   `nombre` varchar(15) DEFAULT NULL,
   `descripcion` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -93,7 +106,7 @@ CREATE TABLE `fletes` (
   `estado` enum('asignada','pendiente','completada','cancelada') DEFAULT NULL,
   `descripcion` varchar(50) DEFAULT NULL,
   `idreclamo` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -109,7 +122,7 @@ CREATE TABLE `reclamos` (
   `idadmin` int(11) DEFAULT NULL,
   `descripcion` varchar(100) NOT NULL,
   `idestado` varchar(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `reclamos`
@@ -130,7 +143,26 @@ INSERT INTO `reclamos` (`id`, `dni`, `fecha`, `serial`, `idadmin`, `descripcion`
 (16, '44765283', '2023-10-14', '12989898', NULL, 'prueba 114', NULL),
 (17, '44765283', '2023-10-14', '12989898', NULL, 'prueba 20', NULL),
 (18, '44765283', '2023-10-14', '12989898', NULL, 'sdadsadsa', NULL),
-(19, '44765283', '2023-10-14', '12321', NULL, 'prueba definitiva', NULL);
+(19, '44765283', '2023-10-14', '12321', NULL, 'prueba definitiva', NULL),
+(20, '44765283', '2023-10-15', '12989898', NULL, 'Prueba nueva, numero 20', NULL),
+(21, '44765283', '2023-10-15', '12989898', NULL, 'se me revento la puerta', NULL),
+(22, '44765283', '2023-10-15', '12321', NULL, 'no anda', NULL),
+(23, '44765283', '2023-10-15', '12989898', NULL, 'jhonathan', NULL),
+(24, '44765283', '2023-10-15', '12321', NULL, 'revento la puerta', NULL),
+(25, '44765283', '2023-10-16', '23132132131', NULL, 'prueba numero nose cuanto, 16/10', NULL),
+(26, '44765283', '2023-10-16', '12989898', NULL, 'aliexpress', NULL),
+(27, '44765283', '2023-10-16', '23132132131', NULL, 'prueba', NULL),
+(28, '44765283', '2023-10-16', '23132132131', NULL, 'revento', NULL),
+(29, '44765283', '2023-10-16', '12989898', NULL, 'nada, es una prueba', NULL),
+(30, '34243', '2023-10-16', '12989898', NULL, 'ensimismada', NULL),
+(31, '44765222', '2023-10-16', '2313213999', NULL, 'dsadsadas', NULL),
+(32, '44765222', '2023-10-16', '2313213999', NULL, 'dsadsadas', NULL),
+(33, '1', '2023-10-16', '44442', NULL, 'se rompio la chaveta', NULL),
+(34, '1', '2023-10-16', '444422131', NULL, 'se rompio la chaveta', NULL),
+(35, '1', '2023-10-16', '444422131', NULL, 'se rompio la chaveta', NULL),
+(36, '1', '2023-10-16', '444422131323', NULL, 'se rompio la chaveta', NULL),
+(37, '1', '2023-10-16', '434', NULL, 'a ver si carga bien la cocina', NULL),
+(38, '1', '2023-10-16', '4344', NULL, 'a ver si carga bien la cocina', NULL);
 
 -- --------------------------------------------------------
 
@@ -146,7 +178,7 @@ CREATE TABLE `servicios` (
   `estado` enum('asignada','pendiente','completada','cancelada') DEFAULT NULL,
   `descripcion` varchar(50) DEFAULT NULL,
   `idreclamo` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -160,7 +192,7 @@ CREATE TABLE `usuarios` (
   `contra` varchar(30) DEFAULT NULL,
   `nombreYapellido` varchar(60) DEFAULT NULL,
   `rol` enum('A','C','T') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -239,7 +271,7 @@ ALTER TABLE `fletes`
 -- AUTO_INCREMENT de la tabla `reclamos`
 --
 ALTER TABLE `reclamos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
