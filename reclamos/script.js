137ae524edaa1d413d45500159154875e5740c78
Lucas Quaroni <lucas.quaroni@enerminds.com>
2023-10-19 10:13:54 -0300

    validacion DNI

--- reclamos/script.js
+++ reclamos/script.js
@@ -1,44 +1,17 @@
-function mostrarFormularioRegistroCliente() {
-  var formularioRegistroCliente = document.getElementById(
-    "formularioRegistroCliente"
-  );
-  var btnValidarCliente = document.getElementById("btnValidarCliente");
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtén el formulario y el campo de entrada de DNI
+  const form = document.querySelector('form');
+  const dniInput = document.querySelector('input[name="dni_cliente"]');
 
-  // Oculta el botón "Validar Cliente" y muestra el formulario de registro
-  btnValidarCliente.style.display = "none";
-  formularioRegistroCliente.style.display = "block";
-}
+  // Agrega un controlador de eventos para el envío del formulario
+  form.addEventListener('submit', function (e) {
+    // Obtén el valor del campo de entrada de DNI
+    const dniValue = dniInput.value.trim();
 
-const prov = [
-  "Buenos Aires",
-  "Catamarca",
-  "Chaco",
-  "Chubut",
-  "Córdoba",
-  "Corrientes",
-  "Entre Ríos",
-  "Formosa",
-  "Jujuy",
-  "La Pampa",
-  "La Rioja",
-  "Mendoza",
-  "Misiones",
-  "Neuquén",
-  "Río Negro",
-  "Salta",
-  "San Juan",
-  "San Luis",
-  "Santa Cruz",
-  "Santa Fe",
-  "Santiago del Estero",
-  "Tierra del Fuego",
-  "Tucumán",
-];
-
-const selectProv = document.getElementById("provincias");
-prov.forEach((provincia) => {
-  const option = document.createElement("option");
-  option.value = provincia;
-  option.textContent = provincia;
-  selectProv.appendChild(option);
+    // Verifica si el DNI tiene entre 7 y 8 números
+    if (!/^\d{7,8}$/.test(dniValue)) {
+      alert('El DNI debe contener entre 7 y 8 números.');
+      e.preventDefault(); // Evita que el formulario se envíe
+    }
+  });
 });
